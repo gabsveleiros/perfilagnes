cd78c728935748f3562f62415c92ef2281f0f84e
Gaby <gabrielly.gaft.veleiros@gmail.com>
2024-09-19 16:38:56 -0300

    upadate icones/button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement;
-
+const accordionHeaders = document.querySelectorAll(".accordion-header");
 //função para alterar o tema
 function changeTheme () {
     const currentTheme = rootHtml.getAttribute("data-theme");
@@ -10,3 +10,12 @@ function changeTheme () {
 }
 toggleTheme.addEventListener("click", changeTheme);
 //fim da função mudar o tema
+
+accordionHeaders.forEach(header =>{
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
+
